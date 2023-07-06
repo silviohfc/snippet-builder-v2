326afa3c118f63de24b9f57b4184c00108fd0533
Silvio Ferreira <silviohfc@gmail.com>
2023-07-06 16:23:32 -0300

    refactor: code style

--- editor.js
+++ editor.js
@@ -1,12 +1,16 @@
-require.config({ paths: { vs: 'monaco-editor/min/vs' } });
+require.config({ paths: { vs: "monaco-editor/min/vs" } });
 
 var editor;
 var didScrollChangeDisposable;
 
-require(['vs/editor/editor.main'], function () {
-  editor = monaco.editor.create(document.getElementById('code-editor'), {
-    value: ['function x() {', '\tconsole.log("Replace me with your code!");', '}'].join('\n'),
-    language: 'javascript',
-    automaticLayout: true
+require(["vs/editor/editor.main"], function () {
+  editor = monaco.editor.create(document.getElementById("code-editor"), {
+    value: [
+      "function x() {",
+      '\tconsole.log("Replace me with your code!");',
+      "}",
+    ].join("\n"),
+    language: "javascript",
+    automaticLayout: true,
   });
 });
--- form.js
+++ form.js
@@ -1,100 +1,106 @@
-$('document').ready(() => {
-  const actualTheme = getTheme()
+$("document").ready(() => {
+  const actualTheme = getTheme();
 
-  if (!actualTheme) saveTheme('light')
+  if (!actualTheme) saveTheme("light");
 
-  updateTheme()
-  updateSavedSnippets()
+  updateTheme();
+  updateSavedSnippets();
 
-  $('#themeSwitch').on('click', function() {
-    const isDarkTheme = $(this).is(':checked')
+  $("#themeSwitch").on("click", function () {
+    const isDarkTheme = $(this).is(":checked");
 
-    isDarkTheme ? saveTheme('dark') : saveTheme('light')
+    isDarkTheme ? saveTheme("dark") : saveTheme("light");
 
-    updateTheme()
-  })
+    updateTheme();
+  });
 
-  $('.list-group button').on('click', function() {
-    const trigger = $(this).text()
-    const snippet = localStorage.getItem(trigger)
+  $(".list-group button").on("click", function () {
+    const trigger = $(this).text();
+    const snippet = localStorage.getItem(trigger);
 
-    if (!snippet) return alert("Snippet not found.")
+    if (!snippet) return alert("Snippet not found.");
 
-    const parsedSnippet = JSON.parse(snippet)
-    const inputTrigger = $('#inputTrigger')
-    const inputDescription = $('#inputDescription')
+    const parsedSnippet = JSON.parse(snippet);
+    const inputTrigger = $("#inputTrigger");
+    const inputDescription = $("#inputDescription");
 
-    inputTrigger.val(trigger)
-    inputDescription.val(parsedSnippet.description)
-    editor.setValue(parsedSnippet.code)
-  })
+    inputTrigger.val(trigger);
+    inputDescription.val(parsedSnippet.description);
+    editor.setValue(parsedSnippet.code);
+  });
 
-  $('#button-save').on('click', function() {
-    const inputTrigger = $('#inputTrigger')
-    const inputDescription = $('#inputDescription')
-    const codeEditor = $('#code-editor')
-    
-    inputTrigger.removeClass('is-invalid')
-    inputDescription.removeClass('is-invalid')
-    codeEditor.removeClass('border-danger')
-  
-    const triggerValue = inputTrigger.val().trim()
-    const descriptionValue = inputDescription.val().trim()
-    const codeValue = editor.getValue()
-  
+  $("#button-save").on("click", function () {
+    const inputTrigger = $("#inputTrigger");
+    const inputDescription = $("#inputDescription");
+    const codeEditor = $("#code-editor");
+
+    inputTrigger.removeClass("is-invalid");
+    inputDescription.removeClass("is-invalid");
+    codeEditor.removeClass("border-danger");
+
+    const triggerValue = inputTrigger.val().trim();
+    const descriptionValue = inputDescription.val().trim();
+    const codeValue = editor.getValue();
+
     if (!triggerValue || !descriptionValue || !codeValue) {
-      if (!triggerValue) inputTrigger.addClass('is-invalid')
-      if (!descriptionValue) inputDescription.addClass('is-invalid')
-      if (!codeValue) codeEditor.addClass('border-danger')
-      return
+      if (!triggerValue) inputTrigger.addClass("is-invalid");
+      if (!descriptionValue) inputDescription.addClass("is-invalid");
+      if (!codeValue) codeEditor.addClass("border-danger");
+      return;
     }
-  
-    inputTrigger.val('')
-    inputDescription.val('')
-    editor.setValue('')
-  
-    localStorage.setItem(triggerValue, JSON.stringify({
-      description: descriptionValue,
-      code: codeValue
-    }))
-  
-    updateSavedSnippets()
-  })
-})
+
+    inputTrigger.val("");
+    inputDescription.val("");
+    editor.setValue("");
+
+    localStorage.setItem(
+      triggerValue,
+      JSON.stringify({
+        description: descriptionValue,
+        code: codeValue,
+      })
+    );
+
+    updateSavedSnippets();
+  });
+});
 
 function updateSavedSnippets() {
-  let savedSnippetElements = ""
+  let savedSnippetElements = "";
 
   for (const trigger of Object.keys(localStorage)) {
-    if (trigger === 'sys_theme_config') continue
-    savedSnippetElements = savedSnippetElements.concat(`<button type="button" class="list-group-item list-group-item-action">${trigger}</button>`)
+    if (trigger === "sys_theme_config") continue;
+    savedSnippetElements = savedSnippetElements.concat(
+      `<button type="button" class="list-group-item list-group-item-action">${trigger}</button>`
+    );
   }
 
-  $('.list-group').html(savedSnippetElements)
+  $(".list-group").html(savedSnippetElements);
 }
 
 function getTheme() {
-  return localStorage.getItem('sys_theme_config')
+  return localStorage.getItem("sys_theme_config");
 }
 
 function saveTheme(theme) {
-  return localStorage.setItem('sys_theme_config', theme)
+  return localStorage.setItem("sys_theme_config", theme);
 }
 
 function updateTheme() {
-  if (!editor) return setTimeout(() => {
-    updateTheme()
-  }, 50);
+  if (!editor)
+    return setTimeout(() => {
+      updateTheme();
+    }, 50);
 
-  const actualTheme = getTheme()
+  const actualTheme = getTheme();
 
-  if (actualTheme === 'light') {
-    $('#themeSwitch').prop('checked', false);
-    editor._themeService.setTheme('vs-light')
-    document.documentElement.setAttribute('data-bs-theme','light')
+  if (actualTheme === "light") {
+    $("#themeSwitch").prop("checked", false);
+    editor._themeService.setTheme("vs-light");
+    document.documentElement.setAttribute("data-bs-theme", "light");
   } else {
-    $('#themeSwitch').prop('checked', true);
-    editor._themeService.setTheme('vs-dark')
-    document.documentElement.setAttribute('data-bs-theme','dark')
+    $("#themeSwitch").prop("checked", true);
+    editor._themeService.setTheme("vs-dark");
+    document.documentElement.setAttribute("data-bs-theme", "dark");
   }
 }
