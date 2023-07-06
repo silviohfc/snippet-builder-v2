33629ca6ad2a6fc6cf011b22f958ccdf0dfb60b8
Silvio Ferreira <silviohfc@gmail.com>
2023-07-06 00:11:26 -0300

    fix: update code editor size on resize

--- editor.js
+++ editor.js
@@ -3,6 +3,7 @@ require.config({ paths: { vs: 'monaco-editor/min/vs' } });
 require(['vs/editor/editor.main'], function () {
   var editor = monaco.editor.create(document.getElementById('code-editor'), {
     value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-    language: 'javascript'
+    language: 'javascript',
+    automaticLayout: true
   });
 });
