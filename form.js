2f778053d26414a8f4b3676ae3e96b057a8b7d24
Silvio Ferreira <silviohfc@gmail.com>
2023-07-06 01:40:24 -0300

    feat: retrieve saved snippets

--- form.js
+++ form.js
@@ -1,37 +1,52 @@
 $('document').ready(() => {
   updateSavedSnippets()
-})
 
-$('#button-save').on('click', () => {
-  const inputTrigger = $('#inputTrigger')
-  const inputDescription = $('#inputDescription')
-  const codeEditor = $('#code-editor')
+  $('.list-group button').on('click', function() {
+    const trigger = $(this).text()
+    const snippet = localStorage.getItem(trigger)
+
+    if (!snippet) return alert("Snippet not found.")
+
+    const parsedSnippet = JSON.parse(snippet)
+    const inputTrigger = $('#inputTrigger')
+    const inputDescription = $('#inputDescription')
+
+    inputTrigger.val(trigger)
+    inputDescription.val(parsedSnippet.description)
+    editor.setValue(parsedSnippet.code)
+  })
+
+  $('#button-save').on('click', function() {
+    const inputTrigger = $('#inputTrigger')
+    const inputDescription = $('#inputDescription')
+    const codeEditor = $('#code-editor')
+    
+    inputTrigger.removeClass('is-invalid')
+    inputDescription.removeClass('is-invalid')
+    codeEditor.removeClass('border-danger')
   
-  inputTrigger.removeClass('is-invalid')
-  inputDescription.removeClass('is-invalid')
-  codeEditor.removeClass('border-danger')
-
-  const triggerValue = inputTrigger.val().trim()
-  const descriptionValue = inputDescription.val().trim()
-  const codeValue = editor.getValue()
-
-  if (!triggerValue || !descriptionValue || !codeValue) {
-    if (!triggerValue) inputTrigger.addClass('is-invalid')
-    if (!descriptionValue) inputDescription.addClass('is-invalid')
-    if (!codeValue) codeEditor.addClass('border-danger')
-    return
-  }
-
-  inputTrigger.val('')
-  inputDescription.val('')
-  editor.setValue('')
-
-  localStorage.setItem(triggerValue, JSON.stringify({
-    description: descriptionValue,
-    code: codeValue
-  }))
-
-  updateSavedSnippets()
+    const triggerValue = inputTrigger.val().trim()
+    const descriptionValue = inputDescription.val().trim()
+    const codeValue = editor.getValue()
+  
+    if (!triggerValue || !descriptionValue || !codeValue) {
+      if (!triggerValue) inputTrigger.addClass('is-invalid')
+      if (!descriptionValue) inputDescription.addClass('is-invalid')
+      if (!codeValue) codeEditor.addClass('border-danger')
+      return
+    }
+  
+    inputTrigger.val('')
+    inputDescription.val('')
+    editor.setValue('')
+  
+    localStorage.setItem(triggerValue, JSON.stringify({
+      description: descriptionValue,
+      code: codeValue
+    }))
+  
+    updateSavedSnippets()
+  })
 })
 
 function updateSavedSnippets() {
