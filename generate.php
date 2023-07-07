f592d676cdcb62a72aa156e297842d84beae13ee
Silvio Ferreira <silviohfc@gmail.com>
2023-07-07 00:49:37 -0300

    fix: return empty object if snippets not exists

--- generate.php
+++ generate.php
@@ -2,6 +2,11 @@
   $className = isset($_POST['className']) ? $_POST['className'] : null;
   $snippets = isset($_POST['snippets']) ? $_POST['snippets'] : null;
 
+  if(!isset($snippets)) {
+    echo json_encode((object)array());
+    return;
+  }
+
   require_once("./generators/".$className.".php");
 
   $instance = new $className;
